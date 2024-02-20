4228a94165c499aaa99daab44018d215280091b8
Anonimous-User <95382802+Anonimous-User@users.noreply.github.com>
2024-02-20 18:13:12 -0500

    added some templates

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,6 +6,7 @@ import java.util.TreeSet;
 import java.util.HashSet;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -15,6 +15,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         InputStreamReader in=new InputStreamReader(System.in);
         BufferedReader br=new BufferedReader(in);
-        Scanner scanner = new Scanner(System.in);
+        
+        // String str = br.readLine();
+        // int smth = Integer.parseInt(br.readLine());
+        // int[] arr = Arrays.stream(br.readLine().trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+        // Queue<Integer> queue = new LinkedList<>();
+        // Stack<Integer> stack = new Stack<>();
+        // ArrayList<Integer> arrList = new ArrayList<>();
+        // Set<Integer> hashSet = new HashSet<>(); //random order, can store Null, faster
+        // Set<Integer> treeSet = new TreeSet<>(); //sorts in order, memory efficient
+        // HashMap<Integer, String> hashmap = new HashMap<>();
     }
 }
+//see 2016 s2 for quick sort, 2020 s1 for binary search (can also use Arrays class)
